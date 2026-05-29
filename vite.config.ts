7586626a2f1009ca07e06e1f26b4e13e2a28ceb2
d1vij <git@divijverma.com>
2026-05-29 08:59:52 +0530

    upload

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({ 
+    server: {
+        allowedHosts: true,
+        host: true
+    },
+    plugins: [tailwindcss(), sveltekit()]
+});
